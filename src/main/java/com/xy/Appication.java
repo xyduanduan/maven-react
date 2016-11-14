93726f04f7ed85472384a4f4ef77a2910dfd8188
xyduan <xyduan@thoughtworks.com>
2016-11-14 21:52:21 +0800

    rename

---
+++ src/main/java/com/xy/Appication.java
@@ -0,0 +1,19 @@
+package com.xy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+/**
+ * Created by xyduan on 11/6/16.
+ */
+@SpringBootApplication
+public class Appication {
+    public  static void main(String[] args ) {
+        SpringApplication.run(Appication.class, args);
+
+
+    }
+
+}
+
